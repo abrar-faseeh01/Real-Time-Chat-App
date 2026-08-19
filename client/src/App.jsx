eb573edf64c934a1af790d0dcca42ac641446316
abrar-faseeh01 <abrarfaseeh01gmail.com>
2026-08-19 23:07:49 +0600

    Improve production background sizing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
   return (
     <div
-      className="bg-contain bg-center bg-no-repeat min-h-screen"
+      className="bg-black bg-cover bg-center bg-no-repeat min-h-screen"
       style={{ backgroundImage: `url(${bgImage})` }}
     >
       <Toaster />
